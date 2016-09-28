77f8d00f0a6b6e905ddb3ae8fad081771c20861e
Justyna <justyna.heinbockel@penna.com>
2016-09-28 17:33:07 +0100

    Added camel case resolver to signalR

--- HomeHUD/App_Start/SignalRConfig.cs
+++ HomeHUD/App_Start/SignalRConfig.cs
@@ -1,14 +1,55 @@
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Owin;
 using SimpleInjector;
 using System;
+using System.Reflection;
 
 namespace HomeHUD
 {
     public partial class SignalRConfig
     {
-        public static void AdjustConnectionTimeouts()
+        public class SignalRCamelCaseContractResolver : IContractResolver
+        {
+            private readonly Assembly _signalRInternalAssembly;
+            private readonly IContractResolver _camelCaseContractResolver;
+            private readonly IContractResolver _defaultContractSerializer;
+
+            public SignalRCamelCaseContractResolver()
+            {
+                _defaultContractSerializer = new DefaultContractResolver();
+                _camelCaseContractResolver = new CamelCasePropertyNamesContractResolver();
+                _signalRInternalAssembly = typeof(Connection).Assembly;
+            }
+
+            public JsonContract ResolveContract(Type type)
+            {
+                return type.Assembly.Equals(_signalRInternalAssembly)
+                    ? _defaultContractSerializer.ResolveContract(type)
+                    : _camelCaseContractResolver.ResolveContract(type);
+            }
+        }
+
+        public static void Configure(IAppBuilder app, Container container)
+        {
+            var config = new HubConfiguration
+            {
+                EnableDetailedErrors = true
+            };
+
+            AdjustConnectionTimeouts();
+
+            var activator = new SimpleInjectorScopedHubActivator(container);
+            GlobalHost.DependencyResolver.Register(typeof(IHubActivator), () => activator);
+            GlobalHost.DependencyResolver.Register(typeof(JsonSerializer), GetSignalRCamelCaseJsonSerializer);
+
+            app.MapSignalR(config);
+        }
+
+        private static void AdjustConnectionTimeouts()
         {
             // Make long polling connections wait a maximum of 110 seconds for a
             // response. When that time expires, trigger a timeout command and
@@ -25,19 +66,14 @@ namespace HomeHUD
             GlobalHost.Configuration.KeepAlive = TimeSpan.FromSeconds(2);
         }
 
-        public static void Configure(IAppBuilder app, Container container)
+        private static JsonSerializer GetSignalRCamelCaseJsonSerializer()
         {
-            var config = new HubConfiguration
+            var settings = new JsonSerializerSettings
             {
-                EnableDetailedErrors = true
+                ContractResolver = new SignalRCamelCaseContractResolver()
             };
 
-            AdjustConnectionTimeouts();
-
-            var activator = new SimpleInjectorScopedHubActivator(container);
-            GlobalHost.DependencyResolver.Register(typeof(IHubActivator), () => activator);
-
-            app.MapSignalR(config);
+            return JsonSerializer.Create(settings);
         }
     }
 }
