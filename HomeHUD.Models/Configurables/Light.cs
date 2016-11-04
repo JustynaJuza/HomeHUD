0c7b95fa35988cc673a0917e14a8ca4f92948a67
Justyna <justyna.heinbockel@penna.com>
2016-11-04 10:09:05 +0000

    Added properties on Light

--- HomeHUD.Models/Configurables/Light.cs
+++ HomeHUD.Models/Configurables/Light.cs
@@ -15,6 +15,8 @@ namespace HomeHUD.Models.Configurables
         public int Id { get; set; }
         public LightSwitchState State { get; set; }
         public string Color { get; set; }
+        public string Brightness { get; set; }
+        public string Description { get; set; }
         public int RoomId { get; set; }
         public Room Room { get; set; }
 
@@ -25,7 +27,9 @@ namespace HomeHUD.Models.Configurables
     {
         public int Id { get; set; }
         public LightSwitchState State { get; set; }
+        public string Description { get; set; }
         public string Color { get; set; }
+        public string Brightness { get; set; }
         public int RoomId { get; set; }
     }
 
