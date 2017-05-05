1f1b02db964e9c3743114f1c159f721f98781848
Justyna <justyna.heinbockel@penna.com>
2017-05-05 17:14:36 +0100

    BROKEN BUILD, adding c# files in progress, unresolved ef conflicts with core

---
+++ HomeHUD.Models/Configurables/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace HomeHUD.Models.Configurables
+{
+    public class Room
+    {
+        public Room()
+        {
+            SortWeight = short.MaxValue;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short SortWeight { get; set; }
+        public string Hash { get; set; }
+        public ICollection<Light> Lights { get; set; }
+    }
+
+    public class RoomViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int sortWeight { get; set; }
+        public string hash { get; set; }
+        public IEnumerable<int> lights { get; set; }
+    }
+}
---
+++ HomeHUD.Models/DbContext/DbContext.EntityConfig.cs
@@ -0,0 +1,21 @@
+﻿using HomeHUD.Models.Users;
+
+namespace HomeHUD.Models.DbContext
+{
+    public partial class ApplicationDbContext
+    {
+        public static class EntityConfig
+        {
+            internal static void ConfigureModelBuilder(System.Data.Entity.DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<User>().ToTable("Users");
+                modelBuilder.Entity<UserRole>().ToTable("UserRoles");
+                modelBuilder.Entity<UserLogin>().ToTable("UserLogins");
+                modelBuilder.Entity<UserClaim>().ToTable("UserClaims");
+                modelBuilder.Entity<Role>().ToTable("Roles");
+
+
+            }
+        }
+    }
+}
