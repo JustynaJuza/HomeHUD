55868a1f6a80952e8535322321c1141d9d7bbf49
Justyna <justyna.heinbockel@gmail.com>
2017-08-22 17:40:05 +0100

    Added global CultureInfo so date is logged in normal format

--- HomeHUD/Program.cs
+++ HomeHUD/Program.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Hosting;
+using System.Globalization;
 using System.IO;
 
 namespace HomeHUD
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
+            var culture = new CultureInfo("en-GB");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
